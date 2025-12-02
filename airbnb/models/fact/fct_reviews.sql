e125ca2375b38a56b6df0cd5297d8e3122a17dc6
Dhony Silva <dhony.dev@gmail.com>
2025-12-02 00:09:46 -0300

    Fix name as code review

--- airbnb/models/fact/fct_reviews.sql
+++ airbnb/models/fact/fct_reviews.sql
@@ -12,7 +12,7 @@ with src_reviews AS (
 select * from src_reviews
 where review_text is not null
 
--- It must be higher than the maximum data
+-- It must be higher than the maximum date
 {% if is_incremental() %}
   and review_date > (select max(review_date) from {{ this }})
 {% endif %}
