3daa3eb9d6e96bcc5475ad5e824a49ff7a86f6db
Dhony Silva <dhony.dev@gmail.com>
2025-12-08 20:40:34 -0300

    Add the standard dbt home folder to read the profile.yml file

--- dagster_entropia/dagster_entropia/project.py
+++ dagster_entropia/dagster_entropia/project.py
@@ -5,5 +5,6 @@ from dagster_dbt import DbtProject
 airbnb_project = DbtProject(
     project_dir=Path(__file__).joinpath("..", "..", "..", "airbnb").resolve(),
     packaged_project_dir=Path(__file__).joinpath("..", "..", "dbt-project").resolve(),
+    profiles_dir=Path.home() / ".dbt"
 )
 airbnb_project.prepare_if_dev()
