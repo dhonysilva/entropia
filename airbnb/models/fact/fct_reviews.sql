9da9a1bafad24325683d1e2311164bc4c3385fec
Dhony Silva <dhony.dev@gmail.com>
2025-12-08 12:12:26 -0300

    Add surrogate key to fact reviews utilizing dbt_utils

--- airbnb/models/fact/fct_reviews.sql
+++ airbnb/models/fact/fct_reviews.sql
@@ -9,8 +9,14 @@ with src_reviews AS (
   select * from {{ ref('src_reviews') }}
 )
 
-select * from src_reviews
-where review_text is not null
+select
+    {{ dbt_utils.generate_surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }}
+    as review_id,
+    *
+from
+    src_reviews
+where
+    review_text is not null
 
 -- It must be higher than the maximum date
 {% if is_incremental() %}
